682bec089495fdff986421577b3fe31d1b106539
RASAM KAMYAR <rasamkamyar@gmail.com>
2024-03-24 14:19:20 +0330

    edit style

--- international-restaurant/components/templates/CategoriesPage/index.js
+++ international-restaurant/components/templates/CategoriesPage/index.js
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import Style from "./index.module.css";
+import { useRouter } from "next/router";
 
 function CategoriesPage() {
+  const [query, setQuery] = useState({
+    difficulty: "",
+    time: "",
+  });
+
+  function changeHandler(e) {
+    setQuery({ ...query, [e.target.name]: e.target.value });
+  }
+
   return (
     <div className={Style.container}>
       <h2>Categories</h2>
       <div className={Style.select}>
-        <select>
-          <option>Difficulty</option>
-          <option>Easy</option>
-          <option>Medium</option>
-          <option>Hard</option>
+        <select name="difficulty" onChange={changeHandler}>
+          <option value="">Difficulty</option>
+          <option value="Easy">Easy</option>
+          <option value="Medium">Medium</option>
+          <option value="Hard">Hard</option>
         </select>
-        <select>
-          <option>Cooking Time</option>
-          <option>More than 30 mins</option>
-          <option>Less than 30 mins</option>
+        <select name="time" onChange={changeHandler}>
+          <option value="">Cooking Time</option>
+          <option value="more">More than 30 mins</option>
+          <option value="less">Less than 30 mins</option>
         </select>
-      <button>Search</button>
+        <button>Search</button>
       </div>
     </div>
   );
